863ecb4400858f43392b679a343196a72621c525
yang <yangzq19931003@outlook.com>
2019-08-22 21:14:54 +0800

    提交作业java step by step 1-6 0822

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,31 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
